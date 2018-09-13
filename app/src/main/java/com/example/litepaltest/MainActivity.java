6aa220a40bef99b9b46bc21e9f26b29bf67dd034
ooxiaoyan <810978527@qq.com>
2018-09-13 18:03:56 +0800

    Query all data from database

--- app/src/main/java/com/example/litepaltest/MainActivity.java
+++ app/src/main/java/com/example/litepaltest/MainActivity.java
@@ -3,13 +3,19 @@ package com.example.litepaltest;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import org.litepal.LitePal;
 import org.litepal.crud.DataSupport;
+import org.litepal.crud.LitePalSupport;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +58,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 DataSupport.deleteAll(Book.class, "price < ?", "15");
+            }
+        });
+
+        Button queryData = findViewById(R.id.query_data);
+        queryData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                List<Book> books = DataSupport.findAll(Book.class);
+                for (Book book : books) {
+                    Log.d(TAG, "book name is " + book.getName());
+                    Log.d(TAG, "book author is " + book.getAuthor());
+                    Log.d(TAG, "book pages is " + book.getPages());
+                    Log.d(TAG, "book price is " + book.getPrice());
+                    Log.d(TAG, "book press is " + book.getPress());
+                }
             }
         });
     }
